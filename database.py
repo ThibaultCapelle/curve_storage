ac5060560bde13b891d46a0ec4c7ac25907ba255
ThibaultCapelle <thibault.capelle@polytechnique.edu>
2018-12-05 19:53:31 +0100

    basic curve exists

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import os, json, time
+import os, json, time, shutil
 from datetime import datetime
 '''
 class CURVE:
@@ -12,7 +12,7 @@ class CURVE:
         database.delete(self.id)
 '''
 
-class database:
+class DataBase:
 
     def __init__(self, database_location=None, data_location=r'C:\Users\Thibault\Documents\phd\python\Database_test'):
         if database_location is None:
@@ -37,15 +37,19 @@ class database:
             return os.path.join(os.getcwd(), 'database.json')
 
     def get_folder_from_id(self, id):
+        data = self.get_data()
+        assert str(id) in data.keys()
+        path = os.path.join(self.data_location,time.strftime("%Y\%M\%d",time.gmtime(data[str(id)])))
+        assert os.path.exists(path)
+        return path
+
+    def get_data(self):
         with open(self.database_location, 'r') as f:
             data=json.load(f)
-        assert str(id) in data.keys()
-        path = time.strftime("%Y\%M\%d",time.gmtime(data[str(id)]))
-        return os.path.join(self.data_location,path)
+        return data
 
     def get_last_id(self):
-        with open(self.database_location, 'r') as f:
-            data=json.load(f)
+        data=self.get_data()
         result = 1
         for k in data.keys():
             if int(k)>result:
@@ -70,7 +74,40 @@ class database:
             os.chdir(str(d.month()))
         if not str(d.day()) in os.listdir():
             os.mkdir(str(d.day()))
+        return new_id
 
+    def remove(self, id):
+        data = self.get_data()
+        data.pop(str(id))
+        with open(self.database_location, 'w') as f:
+            json.dump(data, f)
+
+class Curve:
+
+    def __init__(self, *args, **kwargs):
+        self.database = DataBase()
+        self.id=self.database.new_id()
+        self.directory = self.database.get_folder_from_id(self.id)
+        assert len(args)==2
+        self.x, self.y = args
+        self.params = kwargs
+
+    def exist_directory(self):
+        return str(self.id) in os.listdir(self.directory)
+
+    def get_or_create_dir(self):
+        os.chdir(self.directory)
+        if not str(self.id) in os.listdir():
+            os.mkdir(str(self.id))
+        path = os.path.join(self.directory, str(self.id))
+        assert os.path.exists(path)
+        return path
+
+    def delete(self):
+        self.database.remove(self.id)
+        if self.exist_directory():
+            os.chdir(self.directory)
+            shutil.rmtree('/'+str(id))
 
 
 
