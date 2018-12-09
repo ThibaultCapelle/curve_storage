e1a9ddc2563694ceb2ba7d9935ab52b05bbf44c2
ThibaultCapelle <thibault.capelle@polytechnique.edu>
2018-12-09 18:15:23 +0100

    the hierarchy is now implemented in the gui

--- gui.py
+++ gui.py
@@ -7,7 +7,8 @@ Created on Thu Dec  6 15:11:28 2018
 
 from PyQt5.QtWidgets import (QApplication,
 QMessageBox, QGridLayout, QHBoxLayout, QLabel, QWidget, QVBoxLayout,
-QLineEdit, QTableWidget, QSpinBox, QTableWidgetItem, QAbstractItemView)
+QLineEdit, QTableWidget, QSpinBox, QTableWidgetItem, QAbstractItemView,
+QCheckBox, QTreeWidget, QTreeWidgetItem)
 from PyQt5.QtCore import QRect
 import PyQt5.QtCore as QtCore
 import sys, time, os
@@ -33,39 +34,24 @@ class WindowWidget(QWidget):
         self.layout_global.addLayout(self.layout_right)
         self.layout_left.addLayout(self.layout_show_first)
         self.spinbox_widget = SpinBoxWidget()
-        self.table_widget = TableWidget(self.layout)
+        self.tree_widget = TreeWidget()
         self.param_widget = ParamWidget(self.layout)
         self.layout_global.addWidget(self.param_widget)
         self.show_first_label = QLabel('show first')
         self.layout_show_first.addWidget(self.spinbox_widget)
-        self.layout_left.addWidget(self.table_widget)
+        self.layout_left.addWidget(self.tree_widget)
         self.layout_show_first.addWidget(self.show_first_label)
         self.layout_right.addWidget(self.plot_widget)
-        self.spinbox_changed.connect(self.table_widget.recompute_table)
+        self.spinbox_changed.connect(self.tree_widget.compute)
         self.row_changed.connect(self.plot_widget.plot)
         self.row_changed.connect(self.param_widget.actualize)
-        self.database_changed.fileChanged.connect(self.table_widget.recompute_table)
         self.spinbox_widget.setValue(20)
         self.setLayout(self.layout_global)
         self.setGeometry(QRect(0, 0, 1000, 600))
         self.setMaximumHeight(600)
-        self.table_widget.recompute_table(first_use=True)
         self.show()
         self.move(0,0)
         
-    
-    def recompute_dimensions(self):
-        self._width = 1000
-        self._height = np.sum([self.table_widget.rowHeight(i) for i in range(self.table_widget.length)])
-        self._height = self._height+self.table_widget.horizontalHeader().height()
-        self._height = self._height+\
-        self.table_widget.horizontalScrollBar().height()+\
-        self.spinbox_widget.height()+30
-        self._height= np.min([self._height, 800])
-        self.setGeometry(QRect(self.geometry().x(),
-                         self.geometry().y(),
-                         self._width,
-                         self._height))
         
 class ParamWidget(QTableWidget):
     
@@ -78,8 +64,8 @@ class ParamWidget(QTableWidget):
         self.setMaximumWidth(300)
     
     def actualize(self):
-        row = self.window().table_widget.active_row
-        curve_id = int(self.window().table_widget.item(row, 0).text())
+        item = self.window().tree_widget.active_item
+        curve_id = int(item.data(0,0))
         curve = DataBase().get_curve(curve_id)
         self.clear()
         self.setHorizontalHeaderLabels(['Param', 'Value'])
@@ -91,67 +77,65 @@ class ParamWidget(QTableWidget):
             value = QTableWidgetItem()
             value.setText(str(v))
             self.setItem(i,1,value)
-            
-        
-class TableWidget(QTableWidget):
+
+class TreeWidget(QTreeWidget):
     
-    
-    def __init__(self, layout):
+    def __init__(self):
         super().__init__()
-        self.layout=layout
         self.setColumnCount(3)
         self.length = np.min([len(data.keys()), 20])
-        self.setRowCount(self.length)
-        for i in range(self.length):
-            self.setRowHeight(i, 20)
+        self.setHeaderLabels(['Id', 'Name', 'Date'])
         for i in range(3):
-            self.setColumnWidth(i, 50)
-        self.setHorizontalHeaderLabels(['Id', 'Name', 'Date'])
-        self.verticalHeader().setVisible(False)
-        self.cellPressed.connect(self.cell_clicked)
-        self.currentCellChanged.connect(self.current_cell_changed)
-        self.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.active_row=None
-        self.setMaximumWidth(500)
+            self.setColumnWidth(i,50)
+        self.currentItemChanged.connect(self.current_item_changed)
+        self.compute(first_use=True)
         
-        #self.setSortingEnabled(True)
         
-        #self.setGeometry(QRect(9,8,100,100))
-        #self.setSize(np.sum([self.columnWidth(i) for i in range(3)]),192)
-    
-    def cell_clicked(self, row, column):
-        self.active_row=row
-        
-    def current_cell_changed(self, row, column, previous_row, previous_column):
-        #if previous_row !=-1 and previous_column !=-1:
-        self.active_row=row
+    def current_item_changed(self, item, previous_item):
+        self.active_item = item
         self.window().row_changed.emit()
-            
-    def recompute_table(self, first_use=False):
+
+    def compute(self, first_use=False):
+        self.clear()
         if first_use:
             new_size=20
         else:
             new_size = self.window().spinbox_widget.current_value
         if new_size!=self.length or first_use:
-            DataBase().equalize_with_data()
             data = DataBase().get_data()
-            self.length = np.min([len(data.keys()), new_size])
-            self.setRowCount(self.length)
-            N = len(data.keys())
-            for i, (k, v) in enumerate(data.items()):
-                if np.abs(N-i-1)<self.length:
-                    self.setItem(N-i-1, 0, QTableWidgetItem(k))
-                    date = QTableWidgetItem()
-                    date.setText(time.strftime("%Y/%m/%d %H:%M:%S",time.gmtime(v)))
-                    self.setItem(N-i-1, 2, date)
-                    name = QTableWidgetItem()
-                    name.setText(DataBase().get_curve(k).name)
-                    self.setItem(N-i-1,1, name)
-                    #print('i={:}, k={:}, v={:}'.format(i, k, v))
-            #self.sortItems(2, QtCore.Qt.DescendingOrder)
-            self.window().recompute_dimensions()
+            i=0
+            keys = list(data.keys())
+            N=len(keys)
+            while(self.topLevelItemCount()<new_size):
+                key = keys[N-i-1]
+                value = data[key]
+                curve = DataBase().get_curve(key)
+                if str(curve.parent)==curve.id:
+                    data.pop(key)
+                    item=QTreeWidgetItem()
+                    item.setData(0,0,key)
+                    item.setData(2,0,time.strftime("%Y/%m/%d %H:%M:%S",time.gmtime(value)))
+                    item.setData(1,0, curve.name) 
+                    self.addTopLevelItem(item)
+                    for child in curve.childs:
+                        data = self.add_child(item, data, child)  
+                i=i+1
+            self.sortItems(2,QtCore.Qt.DescendingOrder)
+            
         
+    def add_child(self, item, data, child):
+        child = DataBase().get_curve(child)
+        t = data.pop(str(child.id))
+        child_item=QTreeWidgetItem()
+        child_item.setData(0,0,child.id)
+        child_item.setData(2,0,time.strftime("%Y/%m/%d %H:%M:%S",time.gmtime(t)))
+        child_item.setData(1,0, child.name)
+        item.addChild(child_item)
+        for grandchild in child.childs:
+            data = self.add_child(child, data, grandchild)
+        return data
         
+
 class SpinBoxWidget(QSpinBox):
     
     def __init__(self):
@@ -171,8 +155,8 @@ class PlotWidget(pg.PlotWidget):
         super().__init__()
         
     def plot(self):
-        row = self.window().table_widget.active_row
-        curve_id = int(self.window().table_widget.item(row, 0).text())
+        item = self.window().tree_widget.active_item
+        curve_id = int(item.data(0,0))
         curve = DataBase().get_curve(curve_id)
         self.getPlotItem().clear()
         self.getPlotItem().plot(curve.x, curve.y)
