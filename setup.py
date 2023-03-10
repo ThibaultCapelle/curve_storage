20c01c49aba05c91b5ade8592a78bfe1467642d5
Thibault Capelle <capelle_thibault@riseup.net>
2023-03-10 11:01:50 +0100

    added the pictures for the icons

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(name='curve_storage',
       description='Curve Database manager with a GUI',
       author='Thibault CAPELLE',
       author_email='capelle_thibault@riseup.net',
+      include_package_data=True,
+      package_data={'curve_storage': ['pictures/*.png']},
       packages=['curve_storage'],
 	  package_dir={'curve_storage': 'curve_storage'}
      )
